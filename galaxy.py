3f934b2b30cd6d26f6a4f587fd021e4e344f52a4
MikeSusutZZ <121924073+MikeSusutZZ@users.noreply.github.com>
2023-09-16 03:45:36 +0000

    galaxy didnt have self in a method

--- galaxy.py
+++ galaxy.py
@@ -1,18 +1,19 @@
 import random
 from location import Location
 from planet import Planet
+from asteroids import Asteroid
 class Galaxy:
     def __init__(self, size):
-        self.locations = [[0 for x in range(size)] for y in range(size)]
+        self.locations = [[self.determineLoc() for x in range(size)] for y in range(size)]
 
-    def determineLoc():
+    def determineLoc(self):
         option = random.randint(1, 5)
         if option == 1 or option == 2:
             return Planet()
         elif option == 3 or option == 4:
-            return Location('empty')
+            return Location()
         elif option == 5:
-            return Location('asteriod')
+            return Asteroid()
         else:
             return 'Invalid option'
         
